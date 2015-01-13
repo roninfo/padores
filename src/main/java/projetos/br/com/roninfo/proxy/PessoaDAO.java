8ddb77ff519e116c4cec53838e53e9050396e006
Roni Palacio <roninfo@gmail.com>
2015-01-12 23:38:53 -0200

    Delete PessoaDAO.java

--- src/main/java/projetos/br/com/roninfo/proxy/PessoaDAO.java
+++
@@ -1,9 +0,0 @@
-package projetos.br.com.roninfo.proxy;
-
-class PessoaDAO { 
-	public static Pessoa getPessoaByID(String id){ 
-		System.out.println("select * from PESSOA where id="+id); 
-		return new PessoaImpl(id,"Pessoa " + id); 
-	} 
-}
-
